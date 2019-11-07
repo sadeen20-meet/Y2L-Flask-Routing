be9d36c011be4df3b0fe2c6d3d59c0b2c78172ce
myname21-meet <myname21@meet.mit.edu>
2019-11-07 11:49:03 -0500

    message

--- app.py
+++ app.py
@@ -7,10 +7,21 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 ##### Code here ######
 
+@app.route("/home")
+def home():
+	return render_template("home.html")
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
+@app.route("/store")
+def store():
+	return render_template("store.html")
 
 
 #####################
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
